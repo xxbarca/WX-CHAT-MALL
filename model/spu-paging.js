d2c9f87e0c9e369fdd061d0c6cdd9bbd5c500417
liyang <xxbarca@163.com>
2019-12-28 14:30:44 +0800

    Paging分页对象

---
+++ model/spu-paging.js
@@ -0,0 +1,15 @@
+import { Http } from "../utils/http"
+import { Paging } from '../utils/paging'
+
+class SpuPaging {
+
+    static getLatestPaging() {
+        return new Paging({
+            url: `/spu/latest`
+        }, 3)
+    }
+}
+
+export {
+    SpuPaging
+}
--- utils/paging.js
+++ utils/paging.js
@@ -1,48 +1,85 @@
-import { Http } from "./http"
+
+import {Http} from "./http";
 
 class Paging {
-
+    
     start
     count
     req
     locker = false
     url
+    moreData = true
+    accumulator = []
 
-    constructor(req,  count = 10, start = 0) {
+
+    constructor(req, count = 10, start = 0) {
         this.start = start
         this.count = count
         this.req = req
         this.url = req.url
     }
-    
-    getMoreDate() {
-        if (this._getLocker()) {
+
+    async getMoreData() {
+        if(!this.moreData){
             return
         }
-        this._actualGetData()
+        if(!this._getLocker()){
+            return
+        }
+        const data =await this._actualGetData()
         this._releaseLocker()
+        return data
     }
 
-    _actualGetData() {
-        Http.request({})
+
+    async _actualGetData() {
+        const req = this._getCurrentReq()
+        let paging = await Http.request(req)
+        if(!paging){
+            return null
+        }
+        if(paging.total === 0){
+            return {
+                empty:true,
+                items:[],
+                moreData:false,
+                accumulator:[]
+            }
+        }
+
+        this.moreData = Paging._moreData(paging.total_page, paging.page)
+        if(this.moreData){
+            this.start += this.count
+        }
+        this._accumulate(paging.items)
+        return {
+            empty: false,
+            items: paging.items,
+            moreData: this.moreData,
+            accumulator: this.accumulator
+        }
+    }
+
+    _accumulate(items){
+        this.accumulator = this.accumulator.concat(items)
+    }
+
+    static _moreData(totalPage, pageNum) {
+        return pageNum < totalPage - 1
     }
 
     _getCurrentReq() {
         let url = this.url
         const params = `start=${this.start}&count=${this.count}`
-        // url = /v1/spu/latest + '?' + params
-        // url = /v1/spu/latest?other=abc + '&' + params
-
-        if(url.indexOf("?") !== -1) {
+        if(url.includes('?')){
             url += '&' + params
+            // contains
         } else {
-            url += "?" + params
+            url += '?' + params
         }
         this.req.url = url
         return this.req
     }
-
-
 
     _getLocker() {
         if (this.locker) {
@@ -56,4 +93,8 @@ class Paging {
         this.locker = false
     }
 
+}
+
+export {
+    Paging
 }
