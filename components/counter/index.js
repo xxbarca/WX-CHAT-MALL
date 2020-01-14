79f762e24e23c4aac09d8d34e7ee78bcc0f38e73
liyang <xxbarca@163.com>
2020-01-14 16:19:46 +0800

    l-counter的数量控制属性

--- components/counter/index.js
+++ components/counter/index.js
@@ -1,23 +1,43 @@
-// components/counter/index.js
+
+import { Cart } from '../../models/cart'
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
 
-  },
+    properties: {
+        count: {
+            type: Number,
+            value: Cart.SKU_MIN_COUNT
+        },
+        max: {
+            type: Number,
+            value: Cart.SKU_MAX_COUNT
+        },
+        min: {
+            type: Number,
+            value: Cart.SKU_MIN_COUNT
+        }
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    data: {
 
-  },
+    },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+    methods: {
+        onOverStep(event) {
+            const minOrMaxOut = event.detail.type
+            if (minOrMaxOut === 'overflow_max') {
+                wx.showToast({
+                    icon: "none",
+                    duration: 3000,
+                    title: `超出最大购买数量`
+                })
+            }
+            if (minOrMaxOut === 'overflow_min') {
+                wx.showToast({
+                    icon: 'none',
+                    duration: 3000,
+                    title: `最少需要购买${Cart.SKU_MIN_COUNT}件噢`
+                })
+            }
+        }
+    }
 })
---
+++ models/cart.js
@@ -0,0 +1,11 @@
+
+class Cart {
+    // 最小购买数量
+    static SKU_MIN_COUNT = 1
+    // 最大购买数量
+    static SKU_MAX_COUNT = 77
+}
+
+export {
+    Cart
+}
