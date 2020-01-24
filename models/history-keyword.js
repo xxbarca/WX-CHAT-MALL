25799e0d274eaeae9cd541bf31628ed60a019933
liyang <xxbarca@163.com>
2020-01-24 17:48:16 +0800

    搜索历史保存数据

--- models/history-keyword.js
+++ models/history-keyword.js
@@ -1,4 +1,30 @@
 
 class HistoryKeyword {
 
+    static MAX_ITEM_COUNT = 20
+    keywords = []
+
+    save(keyword) {
+        const items = this.keywords.filter(k => {
+            return k === keyword
+        })
+        // 存在相同的,不保存
+        if (items.length !== 0) {
+            return
+        }
+
+        // 超出数组最大值
+        if (this.keywords.length >= HistoryKeyword.MAX_ITEM_COUNT) {
+            this.keywords.pop()
+        }
+        this.keywords.unshift(keyword)
+    }
+
+    get() {
+        return this.keywords
+    }
+
+    clear() {
+
+    }
 }
