1957549dd1c45227e4f749c29d83764ba02507d9
liyang <xxbarca@163.com>
2020-05-10 18:02:29 +0800

    购物车子项

--- components/cart-item/index.js
+++ components/cart-item/index.js
@@ -1,4 +1,7 @@
 // components/cart-item/index.js
+import {parseSpecValue} from "../../utils/sku"
+import {Cart} from "../../models/cart"
+
 Component({
   
     properties: {
@@ -9,8 +12,38 @@ Component({
 	},
   
     data: {
-
+		online: Boolean,
+	    soldOut: Boolean,
+	    discount: Boolean,
+	    specStr: String,
+	    stock: 0,
+	    skuCount: 0
     },
+	
+	observers: {
+		cartItem: function (cartItem) {
+			if (!cartItem) {
+				return
+			}
+			
+			const specStr = parseSpecValue(cartItem.sku.specs)
+			const discount = cartItem.sku.discount_price ? true : false
+			const soldOut = Cart.isSoldOut(cartItem)
+			const online = Cart.isOnline(cartItem)
+			const skuCount = cartItem.count
+			const stock = cartItem.sku.stock
+			
+			this.setData({
+				online,
+				soldOut,
+				discount,
+				specStr,
+				skuCount,
+				stock
+			})
+			
+		}
+	},
 
   
     methods: {
