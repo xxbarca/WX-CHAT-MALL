786ef9aa42a3c0d4f18ec6a143d57657799147a0
liyang <xxbarca@163.com>
2020-10-20 18:10:22 +0800

    我的页面-收货地址

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,10 +1,12 @@
 // pages/my/my.js
-import {CouponStatus} from "../../core/enum"
+import {AuthAddress, CouponStatus} from "../../core/enum"
 import {Coupon} from "../../components/models/coupon"
+import {promisic} from "../../utils/util"
 
 Page({
 	data: {
-		couponCount: 0
+		couponCount: 0,
+		showDialog: false
 	},
     onLoad: async function (options) {
 		const coupons = await Coupon.getMyCoupons(CouponStatus.AVAILABLE)
@@ -21,7 +23,32 @@ Page({
 		})
 	
 	},
-	onMgrAddress() {
+	async onMgrAddress() {
+		const authStatus = await this.hasAuthorizedAddress()
+		if (authStatus === AuthAddress.DENY) {
+			this.setData({
+				showDialog: true
+			})
+			return
+		}
+		await this.openAddress()
+	},
 	
+	async hasAuthorizedAddress() {
+		const settings = await promisic(wx.getSetting)()
+		const addressSetting = settings.authSetting['scope.address']
+		if (addressSetting === undefined) {
+			return AuthAddress.NOT_AUTH
+		}
+		if (addressSetting === false) {
+			return AuthAddress.DENY
+		}
+		if (addressSetting === true) {
+			return AuthAddress.AUTHORIZED
+		}
+	},
+	
+	async openAddress() {
+		await promisic(wx.chooseAddress)()
 	}
 })
