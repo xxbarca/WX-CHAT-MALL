cd45d8ccb32332e3678775bc7bb3265162948bf8
liyang <xxbarca@163.com>
2020-10-21 12:44:59 +0800

    theme-模版irelia

---
+++ components/theme-irelia/index.js
@@ -0,0 +1,46 @@
+// components/theme-irelia/index.js
+import {randomArray} from "../../utils/random"
+
+Component({
+ 
+    properties: {
+	  theme: Object
+    },
+	data: {
+		h: null,
+		w: null,
+		topImg: null,
+		description: '',
+		spuList: [],
+		randoms: Array
+	},
+	
+	observers: {
+		'theme': function (theme) {
+			this.setData({
+				topImg: theme.extend,
+				descriptions: theme.description.split('#'),
+				spuList: theme.spu_list
+			})
+			const randoms = this.getRandoms(theme.spu_list.length)
+			console.log(randoms)
+			this.setData({
+				randoms: randoms
+			})
+		}
+	},
+	
+	methods: {
+		getRandoms(size) {
+			return randomArray(size)
+		},
+		onLoadImg(event) {
+			const {height, width} = event.detail
+			console.log(height,width)
+			this.setData({
+				h: height,
+				w: width,
+			})
+		}
+	}
+})
